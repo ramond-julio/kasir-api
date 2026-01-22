55ede6b20b10b08a63d0f5766b0b5147c2ded11b
ramondjulio <ramonjulio9999@gmail.com>
2026-01-23 00:46:42 +0700

    adding category api

--- main.go
+++ main.go
@@ -15,10 +15,22 @@ type Produk struct {
 	Stok int   `json:"stok"`
 }
 
+type Categories struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
 var produk = []Produk{
 	{ID: 1, Nama: "Laptop", Harga: 15000000, Stok: 10},
 	{ID: 2, Nama: "Smartphone", Harga: 5000000, Stok: 25},
 	{ID: 3, Nama: "Tablet", Harga: 7000000, Stok: 15},
+}
+
+var categories = []Categories{
+	{ID: 1, Name: "Electronic", Description: "For electronic devices"},
+	{ID: 2, Name: "Clothing", Description: "For clothing items"},
+	{ID: 3, Name: "Books", Description: "For books and reading materials"},
 }
 
 func getProdukByID(w http.ResponseWriter, r *http.Request) {
@@ -95,6 +107,73 @@ func deleteProdukByID(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Produk belum ada", http.StatusNotFound)
 }
 
+func getCategoriesByID(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/categories/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid category ID", http.StatusBadRequest)
+		return
+	}
+
+	for _, c := range categories {
+		if c.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(c)
+			return
+		}
+	}
+
+	http.Error(w, "Category not available", http.StatusNotFound)
+}
+
+func updateCategoriesByID(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/categories/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid category ID", http.StatusBadRequest)
+		return
+	}
+
+	var updatedCategory Categories
+	err = json.NewDecoder(r.Body).Decode(&updatedCategory)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	for i := range categories {
+		if categories[i].ID == id {
+			categories[i] = updatedCategory
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(updatedCategory)
+			return
+		}
+	}
+
+	http.Error(w, "Category belum ada", http.StatusNotFound)
+}
+func deleteCategoriesByID(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/api/categories/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid category ID", http.StatusBadRequest)
+		return
+	}
+
+	for i, c := range categories {
+		if c.ID == id {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(map[string]string{
+				"message": "Category successfully deleted",
+			})
+			categories = append(categories[:i], categories[i+1:]...)
+			return
+		}
+	}
+	http.Error(w, "Category not available", http.StatusNotFound)
+}
+
 func main() {
 	//localhost:8080/health
 	http.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
@@ -126,7 +205,22 @@ func main() {
 		}
 	})
 
-
+	http.HandleFunc("/api/categories", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(categories)
+		} else if r.Method == "POST" {
+			var newCategory Categories
+			err := json.NewDecoder(r.Body).Decode(&newCategory)
+			if err != nil {
+				http.Error(w, "Invalid request", http.StatusBadRequest)
+			}
+			newCategory.ID = len(categories) + 1
+			categories = append(categories, newCategory)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(newCategory)
+		}
+	})
 
 	// GET localhost:8080/produk/{id}
 	// PUT localhost:8080/produk/{id}
@@ -138,6 +232,19 @@ func main() {
 			updateProdukByID(w, r)
 		} else if r.Method == "DELETE" {
 			deleteProdukByID(w, r)
+		}
+	})
+
+	// GET localhost:8080/categories/{id}
+	// PUT localhost:8080/categories/{id}
+	// DELETE localhost:8080/categories/{id}
+	http.HandleFunc("/api/categories/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			getCategoriesByID(w, r)
+		} else if r.Method == "PUT" {
+			updateCategoriesByID(w, r)
+		} else if r.Method == "DELETE" {
+			deleteCategoriesByID(w, r)
 		}
 	})
 
